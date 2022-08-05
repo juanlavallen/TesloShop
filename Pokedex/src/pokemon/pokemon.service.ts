de83c5ee1595266e2a182e7f2657c6da804f706b
Juan Lavallen <juanlavallen02@gmail.com>
2022-08-04 23:14:56 -0300

    create pokemon service completed

--- Pokedex/src/pokemon/pokemon.service.ts
+++ Pokedex/src/pokemon/pokemon.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly model: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    try {
+      const pokemon = await this.model.create(createPokemonDto);
+      return pokemon;
+    } catch (e) {
+      if (e.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon exists in DB ${JSON.stringify(e.keyValue)}`,
+        );
+      }
+      throw new InternalServerErrorException(
+        `Can't create Pokemon - Check server logs`,
+      );
+    }
   }
 
   findAll() {
