6dfc19be94db5ed8ffda94daa3a088af4737fb01
Juan Lavallen <juanlavallen02@gmail.com>
2022-08-22 20:08:05 -0300

    Product entity base added

--- TesloShop/src/products/entities/product.entity.ts
+++ TesloShop/src/products/entities/product.entity.ts
@@ -1,1 +1,10 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column('text', { unique: true })
+  title: string;
+}
