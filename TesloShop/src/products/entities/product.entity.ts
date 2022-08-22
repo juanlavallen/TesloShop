77172bec91f1f46b1cc8de37550ad3740640b78a
Juan Lavallen <juanlavallen02@gmail.com>
2022-08-22 20:17:38 -0300

    Product base entity added

--- TesloShop/src/products/entities/product.entity.ts
+++ TesloShop/src/products/entities/product.entity.ts
@@ -5,6 +5,44 @@ export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column('text', { unique: true })
+  @Column({
+    type: 'text',
+    unique: true,
+  })
   title: string;
+
+  @Column({
+    type: 'numeric',
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
+
+  @Column({
+    type: 'text',
+    array: true,
+  })
+  sizes: string[];
+
+  @Column({ type: 'text' })
+  gender: string;
+
+  //   TODO: Tags - Images
 }
