47e2496baa5900c35adfa871fd77416b85789bec
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-07 20:00:14 -0300

    method uploadFile & interceptor

--- TesloShop/src/files/files.controller.ts
+++ TesloShop/src/files/files.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
 }
