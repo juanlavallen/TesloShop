30dd9c20ac78c00efbf0464387ad285aebae65ac
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-14 22:42:23 -0300

    Services & Controllers - FilesModule

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,32 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileNamer, fileFilter } from './helpers';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get(':type/:image')
+  findOne(@Res() res: Response, @Param('image') image: string) {
+    const path = this.filesService.getStaticImage(image);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -26,6 +40,10 @@ export class FilesController {
   )
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException(`Make sure that file is an image`);
-    return file.originalname;
+
+    const HOST_API = this.configService.get('HOST_API');
+    const URL = `${HOST_API}/files/upload/${file.filename}`;
+
+    return URL;
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  private readonly logger = new Logger('FilesService');
+
+  getStaticImage(image: string) {
+    const path = join(__dirname, '../../static/uploads', image);
+    if (!existsSync(path)) {
+      this.logger.error(`No product found with image ${image}`);
+      throw new BadRequestException(`No product found with image ${image}`);
+    }
+    return path;
+  }
+}
