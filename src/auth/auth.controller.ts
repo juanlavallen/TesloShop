53ac3c6f34a87a4eaf65a90d3082ce36bc02a7d3
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-22 14:34:40 -0300

    private route test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { CreateUserDto, SignInUserDto } from './dto';
+import { GetUser } from './decorators/';
+import { RoleGuard } from './guards/role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +24,16 @@ export class AuthController {
   @Post('sign-in')
   signIn(@Body() signInUserDto: SignInUserDto) {
     return this.authService.signIn(signInUserDto);
+  }
+
+  @Get('private')
+  @SetMetadata('roles', ['ADMIN', 'SUPER_USER'])
+  @UseGuards(AuthGuard(), RoleGuard)
+  privateRoute(@GetUser('email') email: string) {
+    return {
+      ok: true,
+      message: 'Welcome to the private route',
+      data: email,
+    };
   }
 }
