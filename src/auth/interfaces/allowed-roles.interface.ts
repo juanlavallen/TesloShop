8e3a7d29ab75f65463733dd163f416ac24028820
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-22 14:47:35 -0300

    allowed roles interface

---
+++ src/auth/interfaces/allowed-roles.interface.ts
@@ -0,0 +1,5 @@
+export enum AllowedRoles {
+  ADMIN = 'ADMIN',
+  SUPER_USER = 'SUPER_USER',
+  USER = 'USER',
+}
---
+++ src/auth/interfaces/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export { AllowedRoles } from './allowed-roles.interface';
+export { JwtPayload } from './jwt-payload.interface';
